2fd73898d4bfb61d79120100b3eb46f8c6ee420b
luucasviana <jose.lucas.viana@gmail.com>
2023-07-17 16:37:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+/* EFEITO PRA REVELAR COM O INICIO DA PÁGINA */
+
+window.pgr = ScrollReveal();
+
+pgr.reveal('.tag', { duration: 1000 });
+pgr.reveal('.tag figcaption', { duration: 2500 });
+pgr.reveal('.btn', { duration: 2500 });
+
+/* EFEITO PRA REVELAR COM A ROLAGEM DA PÁGINA */
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.sobre h3', { duration: 1000 });
+sr.reveal('.sobre p', { duration: 1000 });
+sr.reveal('.habilidade h3', { duration: 1000 });
+sr.reveal('.habilidade ul i', { duration: 1000 });
+sr.reveal('.lista h3', { duration: 1000 });
+sr.reveal('.card', { duration: 1000 });
+sr.reveal('.footer h3 ', { duration: 1000 });
+sr.reveal('.footer i', { duration: 1000 });
+sr.reveal('.rodape p', { duration: 1000 });
